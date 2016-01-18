9fff25cc959ee4020cad82b86412f7767a183c0b
zavierboyd <zavier@boyd.org.nz>
2016-01-18 16:41:21 +1300

    Original Git

---
+++ htmlpages.py
@@ -0,0 +1,28 @@
+Ad = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ballet</title>
+</head>
+<body>
+    <h1>A BALLET SCHOOL WITH A DIFFERENCE</h1>
+    <h1>PRINCIPAL DOLORES THOMAS SCHOOL OF DANCE</h1>
+    <p>An up to date most successful ballet examination syllabus - </p>
+
+    <p>The emphasis at this school is on the movement of dance to music which can
+    incorporate various styles with the use of ballet technique involving the whole use
+    of the body.
+    Developing artistry with expressive hands and arms.</p>
+
+    <p>Performances on stage and retirement homes, - develops confidence and personality.</p>
+
+    <p>The love of dance brings enjoyment to all.</p>
+
+    <p>Classes from Beginners to Advance levels.</p>
+
+    <p>Fees from $76 per term.</p>
+    <img scr="/images/pic.png">
+</body>
+</html>
+"""
